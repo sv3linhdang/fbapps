76910d72729cd20cec0b88b22645d52acb457004
sv3linhdang <37392123+sv3linhdang@users.noreply.github.com>
2018-06-20 17:55:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,8 @@ function AutoCmt(ID, message, TOKEN){
 function in_array(needle, haystack){
     return haystack.indexOf(needle) !== -1;
 }
-var port = process.env.PORT || 8080,
-    ip   = process.env.IP   || '0.0.0.0';
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+
+const port = process.env.PORT || 3338;
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
